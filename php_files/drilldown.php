56a0dc234f9e31112425bb0017dd48f8e6484315
dhurng <dhurng@gmail.com>
2015-05-02 11:40:39 -0700

    Update drilldown.php

--- php_files/drilldown.php
+++ php_files/drilldown.php
@@ -11,34 +11,39 @@
         <h3>Drill Down</h3>
             <form action="drilldownquery.php" method="post" id="drillform">
                 <div class="col-md-4" id="store">
+                    
+                    
+                    <!-- make base attr active -->
+                    
+                    
                     <span class="section">Store: </span><br/>
                     <input type="checkbox" name="check_list[]" value="store_number">Store#<br/> 
                     <input type="checkbox" name="check_list[]" value="store_street_address">Address<br/>
-                    <input type="checkbox" name="check_list[]" value="city">City<br/>
-                    <input type="checkbox" name="check_list[]" value="store_county">County<br/>
-                    <input type="checkbox" name="check_list[]" value="store_zip">Zip<br/>
-                    <input type="checkbox" name="check_list[]" value="sales_district">District<br/>
-                    <input type="checkbox" name="check_list[]" value="store_state">State<br/>
-                    <input type="checkbox" name="check_list[]" value="sales_region">Region<br/>
+                    <input type="checkbox" name="check_list[]" value="city" checked>City<br/>                    
+                    <input type="checkbox" name="check_list[]" value="store_county" disabled><span id="na">County</span><br/>
+                <input type="checkbox" name="check_list[]" value="store_zip" disabled ><span id="na">Zip</span><br/>
+                    <input type="checkbox" name="check_list[]" value="sales_district" disabled><span id="na">District</span><br/>
+                    <input type="checkbox" name="check_list[]" value="store_state" disabled><span id="na">State</span><br/>
+                    <input type="checkbox" name="check_list[]" value="sales_region" disabled><span id="na">Region</span><br/>
                 </div>
                 <div class="col-md-4" id="product">
                     <span class="section">Product: </span><br/>
                     <input type="checkbox" name="check_list2[]" value="description">Description<br/>
                     <input type="checkbox" name="check_list2[]" value="brand">Brand<br/>
                     <input type="checkbox" name="check_list2[]" value="subcategory">Subcategory<br/>
-                    <input type="checkbox" name="check_list2[]" value="category">Category<br/>
-                    <input type="checkbox" name="check_list2[]" value="department">Department<br/>
+                    <input type="checkbox" name="check_list2[]" value="category" checked>Category<br/>
+                    <input type="checkbox" name="check_list2[]" value="department" disabled><span id="na">Department</span><br/>
                 </div>
                 <div class="col-md-4" id="time">
                     <span class="section">Time: </span><br/>
                     <input type="checkbox" name="check_list3[]" value="date">Date<br/>
                     <input type="checkbox" name="check_list3[]" value="day_of_week">Day Of Week<br/>
                     <input type="checkbox" name="check_list3[]" value="day_number_in_month">Day Number in Month<br/>
-                    <input type="checkbox" name="check_list3[]" value="week_number_in_year">Week Number in Year<br/>
-                    <input type="checkbox" name="check_list3[]" value="Month">Month<br/>
-                    <input type="checkbox" name="check_list3[]" value="quarter">Quarter<br/>
-                    <input type="checkbox" name="check_list3[]" value="fiscal_period">Fiscal Period<br/>
-                    <input type="checkbox" name="check_list3[]" value="year">Year<br/>
+                    <input type="checkbox" name="check_list3[]" value="week_number_in_year" checked>Week Number in Year<br/>
+                    <input type="checkbox" name="check_list3[]" value="Month" disabled><span id="na">Month</span><br/>
+                    <input type="checkbox" name="check_list3[]" value="quarter" disabled><span id="na">Quarter</span><br/>
+                    <input type="checkbox" name="check_list3[]" value="fiscal_period" disabled><span id="na">Fiscal Period</span><br/>
+                    <input type="checkbox" name="check_list3[]" value="year" disabled><span id="na">Year</span><br/>
                 </div>
                     
                 <div class="button"> 
